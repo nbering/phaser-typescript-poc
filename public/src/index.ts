8c792a6ac96a467c7df3acc25417aca6a9b6011d
Nicholas Bering <bering.nicholas@gmail.com>
2019-08-19 16:53:36 -0400

    Flesh-out the scene config just a little more.

--- public/src/index.ts
+++ public/src/index.ts
@@ -1,8 +1,27 @@
 /// <reference types="phaser"/>
 
 const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
     width: 800,
-    height: 600
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 300},
+            debug: false
+        }
+    },
+    scene: {
+        preload: function(this: Phaser.Scene){
+
+        },
+        create: function(this: Phaser.Scene){
+
+        },
+        update: function(this: Phaser.Scene, time: number, delta: number){
+
+        }
+    }
 }
 
 const game = new Phaser.Game(config);
